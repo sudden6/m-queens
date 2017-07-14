a786adbd67c0d2a9155804041410641f6ad7890e
sudden6 <sudden6@users.noreply.github.com>
2017-07-14 22:12:39 +0200

    add a testsuite

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <time.h>
 
+//#define TESTSUITE
+
+#ifdef TESTSUITE
+int n = 17;
+#define N n
 #define MAXN 31
+#else
 #define N 17
+#define MAXN N
+#endif
 
 // align diag values to columns
 #define ALIGN_DIA_R(x, d) ((x) >> (d))
@@ -15,6 +23,7 @@
 int cols, diagl, diagr;
 int posibs[MAXN] = {0}; // Our backtracking 'stack'
 int bit_set[MAXN] = {0};
+
 
 void printChessBoard()
 {
@@ -34,24 +43,23 @@ void printChessBoard()
     }
 }
 
-int nqueens(int n) {
+int nqueens() {
   int q0, q1;
 
   int num = 0;
-  int col_mask = -1 << n; // mask with only the allowed columns set to 1
   //
   // The top level is two fors, to save one bit of symmetry in the enumeration
   // by forcing second queen to
   // be AFTER the first queen.
   //
-  for (q0 = 0; q0 < n - 2; q0++) {
-    for (q1 = q0 + 2; q1 < n; q1++) {
+  for (q0 = 0; q0 < N - 2; q0++) {
+    for (q1 = q0 + 2; q1 < N; q1++) {
       int bit0 = 1 << q0;
       bit_set[0] = bit0;
       int bit1 = 1 << q1;
       bit_set[1] = bit1;
       int d = 0; // d is our depth in the backtrack stack
-      cols = bit0 | bit1 | (-1 << n); // The -1 here is used to fill all 'column' bits after n ...
+      cols = bit0 | bit1 | (-1 << N); // The -1 here is used to fill all 'column' bits after n ...
       diagl = ALIGN_COL_L(bit0, 0) | ALIGN_COL_L(bit1, 1);
       diagr = ALIGN_COL_R(bit0, 0) | ALIGN_COL_R(bit1, 1);
       d = 2; // two queens already placed
@@ -108,14 +116,29 @@ int nqueens(int n) {
   return num * 2;
 }
 
-#ifndef OTHER_MAIN
+int results[17] = {1, 0, 0, 2, 10, 4, 40, 92, 352,
+                   724, 2680, 14200, 73712, 365596,
+                   2279184, 14772512, 95815104};
+
 main(int ac, char **av) {
 
+#ifdef TESTSUITE
+    int i;
+    for(i = 1; i < 17; i++) {
+        double time_diff, time_start; // for measuring calculation time
+        n = i + 1;
+        time_start = clock();
+        int result = nqueens();
+        time_diff = (clock() - time_start); // calculating time difference
+        result == results[i] ? printf("PASS ") : printf("FAIL ");
+        printf("N=%2d, Solutions=%10d, Expected=%10d, Time=%f s\n",n , result, results[i], time_diff / CLOCKS_PER_SEC);
+    }
+#else
   double time_diff, time_start; // for measuring calculation time
   time_start = clock();
-  printf("Number of solution for %d is %d\n", N, nqueens(N));
+  printf("Number of solution for %d is %d\n", N, nqueens());
   time_diff = (clock() - time_start); // calculating time difference
   printf("End Time:             %f seconds \n",
-         time_diff / CLOCKS_PER_SEC); // needed time in seconds
-}
+  time_diff / CLOCKS_PER_SEC); // needed time in seconds
 #endif
+}
