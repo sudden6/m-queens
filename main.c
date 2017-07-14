1408357e7e25713bd29a352f284d7ec02a4e51d7
sudden6 <sudden6@users.noreply.github.com>
2017-07-14 19:49:04 +0200

    working for n=4

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 #include <time.h>
 
 #define MAXN 31
-#define N 17
+#define N 5
+
+// align diag values to columns
+#define ALIGN_DIA_R(x, d) ((x) >> (d))
+#define ALIGN_DIA_L(x, d) ((x) >> (N - 1 - (d)))
+
+// align column value to diag
+#define ALIGN_COL_R(x, d) ((x) << (d))
+#define ALIGN_COL_L(x, d) ((x) << (N - 1 - (d)))
 
 int nqueens(int n) {
   int q0, q1;
-  int cols[MAXN], diagl[MAXN], diagr[MAXN],
-      posibs[MAXN]; // Our backtracking 'stack'
+  int cols, diagl, diagr;
+  int posibs[MAXN]; // Our backtracking 'stack'
   int num = 0;
+  int col_mask = -1 << n; // mask with only the allowed columns set to 1
   //
   // The top level is two fors, to save one bit of symmetry in the enumeration
   // by forcing second queen to
@@ -19,25 +28,23 @@ int nqueens(int n) {
       int bit0 = 1 << q0;
       int bit1 = 1 << q1;
       int d = 0; // d is our depth in the backtrack stack
-      cols[0] =
-          bit0 | bit1 |
-          (-1
-           << n); // The -1 here is used to fill all 'column' bits after n ...
-      diagl[0] = (bit0 << 1 | bit1) << 1;
-      diagr[0] = (bit0 >> 1 | bit1) >> 1;
+      cols = bit0 | bit1 | (-1 << n); // The -1 here is used to fill all 'column' bits after n ...
+      diagl = ALIGN_COL_L(bit0, 0) | ALIGN_COL_L(bit1, 1);
+      diagr = ALIGN_COL_R(bit0, 0) | ALIGN_COL_R(bit1, 1);
+      d = 2; // two queens already placed
 
       //  The variable candidates contains the bitmask of possibilities we still
       //  have to try in a given row ...
-      int candidates = ~(cols[0] | diagl[0] | diagr[0]);
+      int candidates = ~(cols | ALIGN_DIA_L(diagl, d) | ALIGN_DIA_R(diagr, d));
 
-      while (d >= 0) {
+      while (d >= 2) {
         while (candidates) {
           int bit = candidates & -candidates; // The standard trick for getting
                                               // the rightmost bit in the mask
-          int ncols = cols[d] | bit;
-          int ndiagl = (diagl[d] | bit) << 1;
-          int ndiagr = (diagr[d] | bit) >> 1;
-          int other_possible = ~(ncols | ndiagl | ndiagr);
+          int ncols = cols | bit;
+          int nxt_diagl = ALIGN_COL_L(bit, d) | diagl;
+          int nxt_diagr = ALIGN_COL_R(bit, d) | diagr;
+          int nxt_possible = ~(ncols | ALIGN_DIA_L(nxt_diagl, d + 1) | ALIGN_DIA_R(nxt_diagr, d + 1));
           candidates ^= bit; // Eliminate the tried possibility.
 
           // The following is the main additional trick here, as recognizing
@@ -49,19 +56,29 @@ int nqueens(int n) {
           // efficient!
           num += ncols == -1;
 
-          if (other_possible) {
+          if (nxt_possible) {
             if (candidates) { // This if saves stack depth + backtrack
                               // operations when we passed the last possibility
                               // in a row.
-              posibs[d++] = candidates; // Go lower in stack ..
+              d++;
+              posibs[d] = candidates; // Go lower in stack ..
             }
-            cols[d] = ncols;
-            diagl[d] = ndiagl;
-            diagr[d] = ndiagr;
-            candidates = other_possible;
+            cols = ncols;
+            diagl = nxt_diagl;
+            diagr = nxt_diagr;
+            candidates = nxt_possible;
+          } else {
+              int clear_mask = cols & ALIGN_DIA_L(diagl, d) & ALIGN_DIA_R(diagr, d);
+              cols &= ~clear_mask | col_mask;
+              diagl &= ~(ALIGN_COL_L(clear_mask, d));
+              diagr &= ~(ALIGN_COL_R(clear_mask, d));
           }
         }
-        candidates = posibs[--d]; // backtrack ...
+        //TODO: check if col_mask needed
+        d--;
+
+
+        candidates = posibs[d]; // backtrack ...
       }
     }
   }
