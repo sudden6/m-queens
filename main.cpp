e01961bb704026a8a98fdb4c30fb282b4403b444
sudden6 <sudden6@gmx.at>
2018-02-01 18:39:31 +0100

    move to c++ and split preplacement from solver

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 #include <sys/time.h>
 #include <time.h>
 
+#include <vector>
+
 // uncomment to start with n=2 and compare to known results
-//#define TESTSUITE
+#define TESTSUITE
 
 #ifndef N
 #define N 12
@@ -24,47 +26,68 @@
 // get the current wall clock time in seconds
 double get_time() {
   struct timeval tp;
-  gettimeofday(&tp, NULL);
+  gettimeofday(&tp, nullptr);
   return tp.tv_sec + tp.tv_usec / 1000000.0;
 }
 
-uint64_t nqueens(uint_fast8_t n) {
+typedef struct {
+    uint_fast32_t cols; // bitfield with all the used columns
+    uint_fast32_t diagl;// bitfield with all the used diagonals down left
+    uint_fast32_t diagr;// bitfield with all the used diagonals down right
+    int_fast32_t placed;// number of rows where queens are already placed
+} start_condition;
+
+std::vector<start_condition> create_subboards_s1(uint_fast8_t n) {
+    std::vector<start_condition> result;
+
+    if(n < 2) {
+        return  result;
+    }
+
+    //
+    // The top level is two fors, to save one bit of symmetry in the enumeration
+    // by forcing second queen to be AFTER the first queen.
+    //
+    // maximum size needed for storing all results
+    uint_fast16_t num_starts = (n-2)*(n-1);
+    uint_fast16_t start_cnt = 0;
+    result.resize(num_starts);         // preallocate memory
+    #pragma omp simd
+    for (uint_fast8_t q0 = 0; q0 < n - 2; q0++) {
+      for (uint_fast8_t q1 = q0 + 2; q1 < n; q1++) {
+        uint_fast32_t bit0 = 1 << q0;
+        uint_fast32_t bit1 = 1 << q1;
+        result[start_cnt].cols = bit0 | bit1;
+        result[start_cnt].diagl = (bit0 << 2) | (bit1 << 1);
+        result[start_cnt].diagr = (bit0 >> 2) | (bit1 >> 1);
+        result[start_cnt].placed = 2;
+        start_cnt++;
+      }
+    }
+    result.resize(start_cnt); // shrink
+}
+
+uint64_t solve_subboard(uint_fast8_t n, std::vector<start_condition> starts) {
 
   // counter for the number of solutions
   // sufficient until n=29
   uint_fast64_t num = 0;
-  //
-  // The top level is two fors, to save one bit of symmetry in the enumeration
-  // by forcing second queen to be AFTER the first queen.
-  //
-  uint_fast16_t num_starts = (n-2)*(n-1);
-  uint_fast16_t start_cnt = 0;
-  uint_fast32_t start_queens[num_starts][2];
-  #pragma omp simd
-  for (uint_fast8_t q0 = 0; q0 < n - 2; q0++) {
-    for (uint_fast8_t q1 = q0 + 2; q1 < n; q1++) {
-      start_queens[start_cnt][0] = 1 << q0;
-      start_queens[start_cnt][1] = 1 << q1;
-      start_cnt++;
-    }
-  }
+  size_t start_cnt = starts.size();
 
 #pragma omp parallel for reduction(+ : num) schedule(dynamic)
   for (uint_fast16_t cnt = 0; cnt < start_cnt; cnt++) {
     uint_fast32_t cols[MAXN], posibs[MAXN]; // Our backtracking 'stack'
     uint_fast32_t diagl[MAXN], diagr[MAXN];
     int_fast32_t rest[MAXN]; // number of rows left
-    uint_fast32_t bit0 = start_queens[cnt][0]; // The first queen placed
-    uint_fast32_t bit1 = start_queens[cnt][1]; // The second queen placed
     int_fast16_t d = 1; // d is our depth in the backtrack stack
     // The UINT_FAST32_MAX here is used to fill all 'coloumn' bits after n ...
-    cols[d] = bit0 | bit1 | (UINT_FAST32_MAX << n);
+    cols[d] = starts[cnt].cols | (UINT_FAST32_MAX << n);
     // This places the first two queens
-    diagl[d] = (bit0 << 2) | (bit1 << 1);
-    diagr[d] = (bit0 >> 2) | (bit1 >> 1);
+    diagl[d] = starts[cnt].diagl;
+    diagr[d] = starts[cnt].diagr;
 #define LOOKAHEAD 3
     // we're allready two rows into the field here
-    rest[d] = n - 2 - LOOKAHEAD;
+    rest[d] = n - LOOKAHEAD - starts[start_cnt].placed;
 
     //  The variable posib contains the bitmask of possibilities we still have
     //  to try in a given row ...
@@ -175,7 +198,8 @@ int main(int argc, char **argv) {
   for (; i <= N; i++) {
     double time_diff, time_start; // for measuring calculation time
     time_start = get_time();
-    uint64_t result = nqueens(i);
+    std::vector<start_condition> st = create_subboards_s1(i);
+    uint64_t result = solve_subboard(i, st);
     time_diff = (get_time() - time_start); // calculating time difference
     result == results[i - 1] ? printf("PASS ") : printf("FAIL ");
     printf("N %2d, Solutions %18" PRIu64 ", Expected %18" PRIu64
