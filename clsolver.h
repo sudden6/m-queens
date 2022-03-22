c4b1ce2e18ea16daeeca4eb703e4b846fcf85be9
sudden6 <sudden6@users.noreply.github.com>
2022-03-22 23:05:44 +0100

    Workaround https://github.com/KhronosGroup/OpenCL-CLHPP/issues/108

--- clsolver.h
+++ clsolver.h
@@ -2,7 +2,7 @@
 #define CLSOLVER_H
 
 #define CL_HPP_TARGET_OPENCL_VERSION 200
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 200
 #include <CL/cl2.hpp>
 
 #include <cstdlib>
