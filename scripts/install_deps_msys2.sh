6c196bcb47eb1daa043fde779112c0a8484bc080
sudden6 <sudden6@gmx.at>
2023-01-15 23:34:19 +0100

    update dependencies for msys2

--- scripts/install_deps_msys2.sh
+++ scripts/install_deps_msys2.sh
@@ -1,3 +1,3 @@
 # Install dependencies
 pacman --noconfirm -Syuu
-pacman --noconfirm -S mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-autotools base-devel mingw-w64-x86_64-gcc mingw-w64-x86_64-opencl-headers mingw-w64-x86_64-opencl-icd
+pacman --noconfirm -S mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-autotools base-devel mingw-w64-x86_64-gcc mingw-w64-x86_64-opencl-icd mingw-w64-x86_64-opencl-clhpp
